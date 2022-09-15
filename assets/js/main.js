b701a66e23f99eb0a6e7038ddb7a49013c335ed2
Francisco Sosa <fransosa.dev@gmail.com>
2022-09-15 17:42:25 -0300

    2nd | Personalizacion a franciscososa.net

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,47 +31,6 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 
-/*==================== ACCORDION SKILLS ====================*/
-const skillsContent = document.getElementsByClassName('skills__content'),
-      skillsHeader = document.querySelectorAll('.skills__header');
-
-function toggleSkills(){
-    let itemClass = this.parentNode.className;
-
-    for(i = 0; i < skillsContent.length; i++){
-        skillsContent[i].className = 'skills__content skills__close';
-    }
-    if(itemClass === 'skills__content skills__close'){
-        this.parentNode.className = 'skills__content skills__open';
-    }
-}
-
-skillsHeader.forEach((el) => {
-    el.addEventListener('click', toggleSkills);
-});
-
-
-/*==================== QUALIFICATION TABS ====================*/
-const tabs = document.querySelectorAll('[data-target'),
-      tabContents = document.querySelectorAll('[data-content]');
-
-tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-        const target = document.querySelector(tab.dataset.target);
-
-        tabContents.forEach(tabContent => {
-            tabContent.classList.remove('qualification__active');
-        });
-        target.classList.add('qualification__active');
-
-        tabs.forEach(tab => {
-            tab.classList.remove('qualification__active');
-        })
-        tab.classList.add('qualification__active');
-    })
-});
-
-
 /*==================== SERVICES MODAL ====================*/
 const modalViews = document.querySelectorAll('.services__modal'),
       modalBtns = document.querySelectorAll('.services__button')
@@ -109,24 +68,6 @@ let swiperPortfolio = new Swiper('.portfolio__container', {
     },
 });
 
-
-/*==================== TESTIMONIAL ====================*/
-let swiperTestimonial = new Swiper('.testimonial__container', {
-    loop: true,
-    grabCursor: true,
-    spaceBetween: 48,
-
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true
-    },
-    breakpoints:{
-        568:{
-            slidesPerView: 2,
-        }
-    }
-});
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]');

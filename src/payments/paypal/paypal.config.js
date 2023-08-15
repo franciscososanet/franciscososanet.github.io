4292e011f415e9d60ee0638a31865dc366be9cec
franciscososanet <franchovd@gmail.com>
2023-08-14 22:14:22 -0300

    Proceso de pago real en Paypal

--- src/payments/paypal/paypal.config.js
+++ src/payments/paypal/paypal.config.js
@@ -3,4 +3,4 @@ config();
 
 export const PAYPAL_API_CLIENT = process.env.PAYPAL_API_CLIENT;
 export const PAYPAL_API_SECRET = process.env.PAYPAL_API_SECRET;
-export const PAYPAL_API = 'https://api-m.sandbox.paypal.com';
+export const PAYPAL_API = 'https://api.paypal.com';
